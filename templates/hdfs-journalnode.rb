010d18ae804a524f2ca3bc824c08ee106c27e01f
Alex Chistyakov <alexclear@gmail.com>
2015-07-10 10:05:56 +0300

    First commit

---
+++ templates/hdfs-journalnode.rb
@@ -0,0 +1,17 @@
+Eye.application 'hdfs-journalnode-{{ env_name }}' do
+  working_dir '/etc/eye'
+  stdall '/var/log/eye/hdfs-journalnode-{{ env_name }}-stdall.log' # stdout,err logs for processes by default
+  trigger :flapping, times: 10, within: 1.minute, retry_in: 3.minutes
+  check :cpu, every: 10.seconds, below: 100, times: 3 # global check for all processes
+
+  process :journalnode_{{ env_name }} do
+    pid_file '{{ hadoop_var_prefix }}/hdfs_journalnode-{{ env_name }}.pid'
+    start_command 'sudo -u {{ hadoop_user }} {{ hadoop_distr_prefix }}/bin/hdfs journalnode'
+
+    daemonize true
+    start_timeout 10.seconds
+    stop_timeout 5.seconds
+
+  end
+
+end
